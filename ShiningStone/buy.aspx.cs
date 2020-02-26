957b1b4e4c21271857571b051f6e2adfda764401
David Galvan <dgalvan@wgu.edu>
2020-02-25 19:11:59 -0800

    Create and dispose of instances keep memory clean by disposing of instances of classes using the dispose keyword.

--- ShiningStone/buy.aspx.cs
+++ ShiningStone/buy.aspx.cs
@@ -11,21 +11,16 @@ namespace ShiningStone
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-           //DateTime TodaysDate = DateTime.Now;
-           //string FormattedDate = TodaysDate.ToString("MM/dd/yyyy");
-           //Response.Write(FormattedDate);
-           decimal DecimalNumber = (decimal)100.75;
-           float FloatNumber = (float) DecimalNumber;
+           
         }
 
         protected void ButtonSubmitOrder_Click(object sender, EventArgs e)
         {
-            string PhoneNumberText = TextBoxPhoneNumber.Text;
-            //int PhoneNumber = Convert.ToInt32(PhoneNumberText);
-            int PhoneNumber = int.Parse(PhoneNumberText);
-            //float PhoneNumberFloat = Convert.ToSingle(PhoneNumber);
-            float PhoneNumberFloat = float.Parse(PhoneNumberText);
-            string PhoneNumberText2 = PhoneNumber.ToString();
+            using (ShiningStoneDataContext Data = new ShiningStoneDataContext())
+            {
+                Data.SubmitChanges();
+            }
+
         }
     }
 }
