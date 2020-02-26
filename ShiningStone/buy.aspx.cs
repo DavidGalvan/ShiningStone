b5a6dfb3defcb91f1c173c5e0e9e3f5c1dcef006
David Galvan <dgalvan@wgu.edu>
2020-02-25 22:50:38 -0800

    Use advanced logic How to set up advanced logical conditions by using brackets to set the precendence of your conditions

--- ShiningStone/buy.aspx.cs
+++ ShiningStone/buy.aspx.cs
@@ -16,24 +16,13 @@ namespace ShiningStone
 
         protected void ButtonSubmitOrder_Click(object sender, EventArgs e)
         {
-            bool AcceptedTerms = CheckBoxAcceptTerms.Checked;
-            if(!AcceptedTerms == true || !Page.IsValid)
-            {
-                Response.Write("You must accept the terms");
-            }
-            
             string SelectedCountry = DropDownListCountry.SelectedItem.Text;
-            if (SelectedCountry == "Canada")
+            if ((Page.IsValid
+                && CheckBoxAcceptTerms.Checked)
+                && (SelectedCountry == "United States"
+                || SelectedCountry == "Mexico"))
             {
-                Response.Write("We can not deliver to Canada");
-            }
-            else if (SelectedCountry == "United States" || SelectedCountry == "Mexico")
-            {
-                Response.Write("Eligible for free delivery");
-            }
-            else
-            {
-                Response.Write("Standard delivery");
+                Response.Write("Logic Succeeded");
             }
         }
     }
